839a84b1be5bbae312c56ef4c3a9c8267d97cad2
limonadev <limonadev@whiteleaf.dev>
2020-11-12 17:50:28 -0500

    Added simultaneous request to test

--- query.py
+++ query.py
@@ -1,3 +1,21 @@
+'''import threading
+import requests
+
+url = 'http://127.0.0.1:8000/code'
+
+f = open('test.py', 'r')
+content = ''.join(f.readlines())
+f.close()
+
+def thread_function():
+    response = requests.post(url, data = content)
+
+thread1 = threading.Thread(target=thread_function)
+thread2 = threading.Thread(target=thread_function)
+
+thread1.start()
+thread2.start()'''
+
 import requests
 
 url = 'http://127.0.0.1:8000/code'
