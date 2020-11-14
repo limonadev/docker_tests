71c1a53363cb67dd66daebbefc28e739eea89747
limonadev <limonadev@whiteleaf.dev>
2020-11-14 07:32:34 -0500

    Added new url

--- base_tests/query.py
+++ base_tests/query.py
@@ -1,7 +1,7 @@
-'''import threading
+import threading
 import requests
 
-url = 'http://127.0.0.1:8000/code'
+url = 'http://34.69.153.204/code'
 
 f = open('test.py', 'r')
 content = ''.join(f.readlines())
@@ -9,20 +9,21 @@ f.close()
 
 def thread_function():
     response = requests.post(url, data = content)
+    print(response.text)
 
 thread1 = threading.Thread(target=thread_function)
 thread2 = threading.Thread(target=thread_function)
 
 thread1.start()
-thread2.start()'''
+thread2.start()
 
-import requests
+'''import requests
 
-url = 'http://127.0.0.1:8000/code'
+url = 'http://34.69.153.204/code'
 
 f = open('test.py', 'r')
 content = ''.join(f.readlines())
 f.close()
 
 r = requests.post(url, data = content)
-print(r.text)
+print(r.text)'''
